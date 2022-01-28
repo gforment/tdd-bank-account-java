a41e203e6deaa29190c59ec15adbe47101e59e5a
Giovanni Formenti <giovanni.formenti@gmail.com>
2022-01-28 14:10:26 +0100

    blank project

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -7,9 +7,5 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
-    @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
-    }
+
 }
